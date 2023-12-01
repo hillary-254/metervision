de90d684e73ed49fdfda1ae5ea8eed4c515e8e25
hillary-254 <123866475+hillary-254@users.noreply.github.com>
2023-12-01 23:24:17 +0300

    started implementing image Model

--- metervision/ocr_app/models.py
+++ metervision/ocr_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class Ocr(models.Model):
+    image_url = models.ImageField(null=True, blank=True, upload_to="images/")
--- metervision/ocr_app/views.py
+++ metervision/ocr_app/views.py
@@ -4,26 +4,33 @@ from PIL import Image
 import pytesseract
 import easyocr
 import numpy as np
+import os
+from django.conf import settings
+from .models import Ocr
 
 
 def ocr(request):
     """This module calls pytesseract and easyocr"""
     if request.method == 'POST' and request.FILES['image']:
         image = request.FILES['image'].file
-        selected_ocr = request.POST.get('ocr_type', 'pytesseract')  # Default to pytesseract if not specified
+        selected_ocr = request.POST.get('ocr_type', 'pytesseract')
         custom_options = {'config': '--psm 6 outputbase digits'}
 
         if selected_ocr == 'pytesseract':
             text = pytesseract_ocr(image, custom_options)
-            return render(request, 'ocr_app/index.html', {'text': text})
+            image_url = save_uploaded_image(image)
+            return render(request, 'ocr_app/index.html', {'text': text, 'image_url': image_url})
         elif selected_ocr == 'easyocr':
             results = easy_ocr(image)
-            return render(request, 'ocr_app/index.html', {'image': image, 'results': results})
+            image_url = save_uploaded_image(image)
+            return render(request, 'ocr_app/index.html', {'image_url': image_url, 'results': results})
         elif selected_ocr == 'both':
             text = pytesseract_ocr(image, custom_options)
             results = easy_ocr(image)
-            return render(request, 'ocr_app/index.html', {'text': text, 'image': image, 'results': results})
-    return render(request, 'ocr_app/index.html')
+            image_url = save_uploaded_image(image)
+            return render(request, 'ocr_app/index.html', {'text': text, 'image_url': image_url, 'results': results})
+    return render(request, 'ocr_app/index.html', {'image_url': None})
+
 
 
 def pytesseract_ocr(image, custom_options=None):
@@ -42,3 +49,11 @@ def easy_ocr(image):
     reader = easyocr.Reader(['en'], gpu=False)  # Specify language and cpu mode
     results = ' '.join([item[1] for item in reader.readtext(np.array(image_data))])  # Convert image to numpy array
     return results
+
+
+def save_uploaded_image(image):
+    # Save the uploaded image to a temporary location
+    image_data = Image.open(image)
+    image_path = os.path.join(settings.MEDIA_ROOT, 'uploaded_image.jpg')
+    image_data.save(image_path)
+    return image_path
